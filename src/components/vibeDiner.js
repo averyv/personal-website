5f9053c36be468cf9bb4edbb3ab64069f886f3c8
Avery Vanacore <avery.e.vanacore.20@dartmouth.edu>
2020-04-09 21:15:52 -0400

    real stuff

--- src/components/vibeDiner.js
+++ src/components/vibeDiner.js
@@ -1,13 +1,10 @@
 import React from 'react';
-
-import {Layout, Card, Typography} from 'antd';
-import {Link} from 'react-router-dom';
-// import 'antd/dist/antd.css';
+import { Layout, Typography} from 'antd';
 import NavBar from './navBar';
 import FooterBar from './footer';
-const { Header, Footer, Sider, Content } = Layout;
-const { Title, Paragraph, Text } = Typography;
-const {Meta} = Card;
+
+const { Content } = Layout;
+const { Title, Paragraph } = Typography;
 
 class VibeDiner extends React.Component {
   render() {
@@ -15,48 +12,40 @@ class VibeDiner extends React.Component {
     <div className="App">
       <Layout>
         <NavBar page="work"/>
-        <Layout id="vibe-diner-container">
-        <Content className="page-content" >
-        
-        <Typography id="vibe-diner-description">
-            <Title>
-                VibeDiner - React, Redux, CSS
-            </Title>
-            <Paragraph className="project-description">
-            VibeDiner was created as a team based final project for CS52: Full-Stack Web Development at Dartmouth College.  
-                In three weeks, my four teammates and I built the front-end UI (using Ant Design) and server-side API for this site, then presented the final product to professors, teaching assistants, and other interested students at the termly Technigala.
+          <Layout id="vibe-diner-container">
+            <Content className="page-content" >
+              <Typography id="vibe-diner-description">
+                <Title>
+                  VibeDiner - React, Redux, CSS
+                </Title>
+                <Paragraph className="project-description">
+                  VibeDiner was created as a team based final project for CS52: Full-Stack Web Development at Dartmouth College.  
+                  In three weeks, my four teammates and I built the front-end UI (using Ant Design) and server-side API for this site, then presented the final product to professors, teaching assistants, and other interested students at the termly Technigala.
                 </Paragraph>
                 <Paragraph className="project-description">
-                The app is a platform that facilitates musical collaboration.  We recognized that many individuals have specific musical talents,
-                but not many have every skill needed to produce a song.  On VibeDiner, users can build profiles and post their current projects to attract other artists who
-                have the skills that the original poster is lacking. 
-                Users can scroll through these projects and filter them by relevant tags and, if interested, they can chat with each other about the project.
-                I individually built the messaging component of the site using a websocket based server for realtime updates, and also contributed to many other features.
+                  The app is a platform that facilitates musical collaboration.  We recognized that many individuals have specific musical talents,
+                  but not many have every skill needed to produce a song.  On VibeDiner, users can build profiles and post their current projects to attract other artists who
+                  have the skills that the original poster is lacking. 
+                  Users can scroll through these projects and filter them by relevant tags and, if interested, they can chat with each other about the project.
+                  I individually built the messaging component of the site using a websocket based server for realtime updates, and also contributed to many other features.
                 </Paragraph>
                 <Paragraph className="project-description">
-                    The credentials for a test account are: email: test, password: test.
-                    </Paragraph>
-                    <Paragraph>
-              
-        <a href="http://vibediner.surge.sh/">
-            URL to hosted page.
-        </a>
-        
-                    </Paragraph>
-               
-        
-            
-        </Typography>
-
-        <div className="website-screenshots" id="vibediner-screenshots">
-          <img alt="sorting" src={require("../img/vibe-diner-home.png")} />
-          <img alt="sorting" src={require("../img/vibe-diner-feed.png")} />
-          <img alt="home tab" src={require("../img/vibe-diner-profile.png")}/>
-          <img alt="progress chart" src={require("../img/vibe-diner-messages.png")} />
-        </div>
-        </Content>
-        
-        </Layout>
+                  The credentials for a test account are: email: test, password: test.
+                </Paragraph>
+                <Paragraph>
+                  <a href="http://vibediner.surge.sh/">
+                    URL to hosted page.
+                  </a>
+                </Paragraph>    
+              </Typography>
+              <div className="website-screenshots" id="vibediner-screenshots">
+                <img alt="vibe diner home" src={require("../img/vibe-diner-home.png")} />
+                <img alt="vibe diner feed" src={require("../img/vibe-diner-feed.png")} />
+                <img alt="vibe diner profile" src={require("../img/vibe-diner-profile.png")}/>
+                <img alt="vibe diner messages" src={require("../img/vibe-diner-messages.png")} />
+              </div>
+            </Content>
+          </Layout>
         <FooterBar/>
       </Layout>
     </div>
